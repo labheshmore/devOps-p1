cade85a52849c9d0b4d3d0efcd07c6759448c288
labhesh more <labheshmore5@gamil.com>
2024-04-18 02:56:13 +0530

    new change

--- provider.tf
+++ provider.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 provider "aws" {
-    region = "us-east-2"
+  region = "us-east-2"
 }
 resource "aws_instance" "first_ec2" {
   ami                     = "ami-08978028fd061067a"
