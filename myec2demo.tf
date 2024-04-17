748e80cb6f9b840e1fce9099e9127595fde18c7a
labhesh more <labheshmore5@gamil.com>
2024-04-18 03:03:35 +0530

    new change 2

--- myec2demo.tf
+++ myec2demo.tf
@@ -10,11 +10,11 @@ provider "aws" {
   region = "us-east-2"
 }
 resource "aws_instance" "first_ec2" {
-  ami                     = "ami-08978028fd061067a"
+  ami                     = "ami-ami-0cd59ecaf368e5ccf "
   instance_type           = "t2.micro"
   key_name                =  "terraform_key" 
   tags = {
-    Name = "Myterraformserver"
+    Name = "terraformserver"
   } 
 }
 
